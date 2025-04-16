8f6f02a195278e7300ce7de4e13a958a9c7a37c4
zack2141 <wilmar.cardona7168@ucaldas.edu.co>
2025-04-16 11:52:38 -0500

    configuracion de la vista de registro del paciente

--- frontend_historias_clinicas/src/app/barra-navegacion-footer/barra-navegacion-footer.component.ts
+++ frontend_historias_clinicas/src/app/barra-navegacion-footer/barra-navegacion-footer.component.ts
@@ -1,10 +1,11 @@
 import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InicioSecionComponent } from '../inicio-secion/inicio-secion.component';
 
 @Component({
   selector: 'app-barra-navegacion-footer',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InicioSecionComponent],
   templateUrl: './barra-navegacion-footer.component.html',
   styleUrl: './barra-navegacion-footer.component.css'
 })
@@ -13,16 +14,16 @@ export class BarraNavegacionFooterComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
 
-    this.interaccion_navegacion(2);
+    this.interaccion_navegacion(1);
   }
 
 
   // Declaracion de variables
 
-  @ViewChild('paciente') paciente!: ElementRef;
-  @ViewChild('medico') medico!: ElementRef;
-  @ViewChild('Recep') Recep!: ElementRef;
-  @ViewChild('Loguin') log!: ElementRef;
+  @ViewChild('paciente') paciente!: ElementRef; // para elementos propios del html
+  @ViewChild('medico') medico!: ElementRef; // para elementos propios del html
+  @ViewChild('Recep') Recep!: ElementRef; // para elementos propios del html
+  @ViewChild('Loguin', { read: ElementRef }) log!: ElementRef; //para leer modulos externos
 
   // constructor 
 
@@ -49,6 +50,10 @@ export class BarraNavegacionFooterComponent implements AfterViewInit {
       case 4:
         this.barra_Recep()
         break
+      
+      case 4:
+        this.ver_registro_paciente()
+        break
     }
   }
 
@@ -59,6 +64,12 @@ export class BarraNavegacionFooterComponent implements AfterViewInit {
     this.renderer.setStyle(this.medico.nativeElement,'display','none');
     this.renderer.setStyle(this.Recep.nativeElement,'display','none');
     this.renderer.setStyle(this.log.nativeElement,'display','block');
+
+  }
+
+  ver_registro_paciente(){
+
+    this.renderer.setStyle(this.log.nativeElement,'display','none');
 
   }
 
