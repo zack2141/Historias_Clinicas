5bbee5bb9be0238ca6b87f59dbe9ec01bea20dbd
zack2141 <wilmar.cardona7168@ucaldas.edu.co>
2025-04-16 10:45:10 -0500

    creación de las barra de nevaegación de los actores

--- frontend_historias_clinicas/src/app/barra-navegacion-footer/barra-navegacion-footer.component.ts
+++ frontend_historias_clinicas/src/app/barra-navegacion-footer/barra-navegacion-footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-barra-navegacion-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './barra-navegacion-footer.component.html',
   styleUrl: './barra-navegacion-footer.component.css'
 })
