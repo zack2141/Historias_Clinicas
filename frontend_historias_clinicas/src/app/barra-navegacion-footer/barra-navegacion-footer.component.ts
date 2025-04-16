92a1872a22e56d0c9c628256c6e6608cca541415
zack2141 <wilmar.cardona7168@ucaldas.edu.co>
2025-04-16 11:41:30 -0500

    implementacion y prueba de barras de navegacion

--- frontend_historias_clinicas/src/app/barra-navegacion-footer/barra-navegacion-footer.component.ts
+++ frontend_historias_clinicas/src/app/barra-navegacion-footer/barra-navegacion-footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,91 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './barra-navegacion-footer.component.html',
   styleUrl: './barra-navegacion-footer.component.css'
 })
-export class BarraNavegacionFooterComponent {
+export class BarraNavegacionFooterComponent implements AfterViewInit {
+
+
+  ngAfterViewInit(): void {
+
+    this.interaccion_navegacion(2);
+  }
+
+
+  // Declaracion de variables
+
+  @ViewChild('paciente') paciente!: ElementRef;
+  @ViewChild('medico') medico!: ElementRef;
+  @ViewChild('Recep') Recep!: ElementRef;
+  @ViewChild('Loguin') log!: ElementRef;
+
+  // constructor 
+
+  constructor(private renderer: Renderer2) {}
+  
+  
+  
+  // Funciones
+  interaccion_navegacion(num:number){
+
+    switch(num){
+      case 1:
+        this.ver_Loguins()
+        break
+      
+      case 2:
+        this.barra_paciente()
+        break
+
+      case 3:
+        this.barra_medico()
+        break
+
+      case 4:
+        this.barra_Recep()
+        break
+    }
+  }
+
+
+  ver_Loguins(){
+
+    this.renderer.setStyle(this.paciente.nativeElement,'display','none');
+    this.renderer.setStyle(this.medico.nativeElement,'display','none');
+    this.renderer.setStyle(this.Recep.nativeElement,'display','none');
+    this.renderer.setStyle(this.log.nativeElement,'display','block');
+
+  }
+
+  
+
+  barra_paciente(){
+
+    this.renderer.setStyle(this.paciente.nativeElement,'display','block');
+    this.renderer.setStyle(this.medico.nativeElement,'display','none');
+    this.renderer.setStyle(this.Recep.nativeElement,'display','none');
+    this.renderer.setStyle(this.log.nativeElement,'display','none');
+
+  }
+
+  barra_medico(){
+
+    this.renderer.setStyle(this.paciente.nativeElement,'display','none');
+    this.renderer.setStyle(this.medico.nativeElement,'display','block');
+    this.renderer.setStyle(this.Recep.nativeElement,'display','none');
+    this.renderer.setStyle(this.log.nativeElement,'display','none');
+
+
+
+  }
+
+  barra_Recep(){
+
+    this.renderer.setStyle(this.paciente.nativeElement,'display','none');
+    this.renderer.setStyle(this.medico.nativeElement,'display','block');
+    this.renderer.setStyle(this.Recep.nativeElement,'display','none');
+    this.renderer.setStyle(this.log.nativeElement,'display','none');
+
+
+
+  }
 
 }
