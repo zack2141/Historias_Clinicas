1acee81d4d454d3be3067f30b14e4a1b985d1ae9
guarmor <ortjuan546@gmail.com>
2025-04-16 22:35:26 -0500

    wilmar avise

--- frontend_historias_clinicas/src/app/servicios/medico.service.ts
+++ frontend_historias_clinicas/src/app/servicios/medico.service.ts
@@ -1,9 +1,26 @@
+import { Observable } from 'rxjs';
+import { HttpParams, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { formatDate } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MedicoService {
 
-  constructor() { }
+  private UrlMedico: string = 'http://localhost:8080/Medico'; 
+
+  constructor(private http: HttpClient) { }
+
+  listaMedicosDisponibles(fecha: Date, hora: string, cargo: string): Observable<any[]> {
+    const fechaFormateada = formatDate(fecha, 'yyyy-MM-dd', 'en-US');
+
+    const params = new HttpParams()
+      .set('fecha', fechaFormateada)
+      .set('hora', hora)
+      .set('cargo', cargo);
+
+    return this.http.get<any[]>(`${this.UrlMedico}/medicosDisponibles`, { params });
+  }
+
 }
