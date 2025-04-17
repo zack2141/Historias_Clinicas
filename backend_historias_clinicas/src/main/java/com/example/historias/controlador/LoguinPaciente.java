cb05e99351155dd668ee0b179f0ebe534241702d
zack2141 <wilmar.cardona7168@ucaldas.edu.co>
2025-04-17 10:38:47 -0500

    nueva conexion con railway, cambios en el croosOrigins de los controladores

--- backend_historias_clinicas/src/main/java/com/example/historias/controlador/LoguinPaciente.java
+++ backend_historias_clinicas/src/main/java/com/example/historias/controlador/LoguinPaciente.java
@@ -15,7 +15,7 @@ import com.example.historias.modelo.paciente;
 
 @RestController
 @RequestMapping("/paciente")
-@CrossOrigin(origins = "http://localhost:8080/")
+@CrossOrigin(origins = "http://localhost:4200/")
 
 public class LoguinPaciente {
     
