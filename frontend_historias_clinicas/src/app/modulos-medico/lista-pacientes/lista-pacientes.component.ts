f54c1e2a356ea01fa0c4cb51ccaaecb205748714
guarmor <ortjuan546@gmail.com>
2025-04-18 11:22:43 -0500

    nuevo cambio

--- frontend_historias_clinicas/src/app/modulos-medico/lista-pacientes/lista-pacientes.component.ts
+++ frontend_historias_clinicas/src/app/modulos-medico/lista-pacientes/lista-pacientes.component.ts
@@ -1,12 +1,14 @@
+import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-lista-pacientes',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './lista-pacientes.component.html',
   styleUrl: './lista-pacientes.component.css'
 })
 export class ListaPacientesComponent {
+fechaSeleccionada: any;
 
 }
