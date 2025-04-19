31ff12970b6d8f0d6fd9fc595e8c24bfb6040565
Brame2003 <kanrishabrameir@gmail.com>
2025-04-19 10:45:03 -0500

    Frotend_Salinas2

--- frontend_historias_clinicas/src/app/modulos-recepcionista/agendar-citas/agendar-citas.component.ts
+++ frontend_historias_clinicas/src/app/modulos-recepcionista/agendar-citas/agendar-citas.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CitaService } from '../../servicios/cita.service';
+import { MedicoService } from '../../servicios/medico.service';
+import { Medico } from '../../entidades/medico';
+import { Paciente } from '../../entidades/paciente';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-agendar-citas',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './agendar-citas.component.html',
   styleUrl: './agendar-citas.component.css'
 })
-export class AgendarCitasComponent {
+
+export class AgendarCitasComponent implements OnInit {
+
+  ngOnInit(): void {
+    
+  }
+
+  constructor(private ServiceCita:CitaService,
+    private ServiceMedico:MedicoService
+  ){}
+
+medico!:Medico[];
+paciente: Paciente = new Paciente;
+fecha!:Date;
+hora!:string;
+motivo!:string;
+cargo!:string;
+
+
+
+solicitar_cita_paciente(){
+  this.ServiceCita.agendar_cita_recep(this.fecha, this.hora, this.motivo, this.medico,this.paciente).subscribe(dato=>{
+    
+  })
+}
+
+medico_encontrado(){
+  this.ServiceMedico.listaMedicosDisponibles(this.fecha, this.hora, this.cargo).subscribe(dato=>{
+
+  })
+}
 
 }
+
